6f1571a774ab431f0a8fceb8d7a105696a76d4d6
Tahiana <tahianarakotolobo@gmail.com>
2022-04-11 15:00:17 +0300

    beneficeresto

--- src/app/benefitsadmin/benefitsadmin.component.ts
+++ src/app/benefitsadmin/benefitsadmin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestaurantService } from '../service/restaurant.service';
 
 @Component({
   selector: 'app-benefitsadmin',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BenefitsadminComponent implements OnInit {
 
-  constructor() { }
+  filtre = 'mois';
+  data = new Array();
+
+  constructor(private router: Router, private restoService:RestaurantService) { }
 
   ngOnInit(): void {
+    this.filtrer();
+  }
+
+  filtrer(){
+    console.log('signup function');
+    var response = true;
+    const success = (response: any = []) => {
+      console.log("response", response);
+      this.data = response.data;
+      this.router.navigateByUrl('/accueil');
+    };
+    const error = (response: any = []) => {
+      console.log("Erreur", response);
+    }
+    this.restoService.filtrer(this.filtre, Number(localStorage.getItem('id'))).subscribe(success, error);
+
   }
 
 }
